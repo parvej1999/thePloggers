4e56344cde1a50cb6a1204be9773c7fc6870a6ff
parvej khan <1999monustp@gmail.com>
2020-06-16 04:26:39 +0530

    updated urls

--- my_project/urls.py
+++ my_project/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', include('blog.urls', namespace='blogs')),
+    path('', include('blog.urls', namespace='blogs')),
     path("user/register/", users_views.register, name="userRegistration"),
     path('user/profile/', users_views.profile, name='profile'),
     path('user/login/',
